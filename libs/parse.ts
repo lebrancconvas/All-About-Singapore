3b2263c1b17b681013bc2093891b9bf3549b47dc
lebrancconvas <lebrancconvas@gmail.com>
2025-10-29 14:48:45 +0700

    ✨ feat(data): update parse data and update data.

--- libs/parse.ts
+++ libs/parse.ts
@@ -5,7 +5,7 @@ import { CONFIG } from "../config";
 export async function convertCSVtoJSON(csvFileName: string) {
   const pathToCSVFile = path.join(...CONFIG.CSV_DATA_PATH, `${csvFileName}.csv`);
   const data = await fs.readFile(pathToCSVFile, { encoding: "utf-8" });
-  const rows = data.split("\n").map(row => row.split(","));
+  const rows = data.trim().split("\n").map(row => row.split(","));
   const headingRows = rows[0];
   const dataRows = rows.splice(1, rows.length);
 
