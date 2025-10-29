bbf8fc74aabc1414d10a9b5769f5b787e018a75f
lebrancconvas <lebrancconvas@gmail.com>
2025-10-29 16:46:00 +0700

    ✨ feat: update get train line data.

--- index.ts
+++ index.ts
@@ -3,8 +3,16 @@ import {
   CSVtoJSONFile 
 } from "./libs";
 
-async function main() {
+import fs from "fs/promises";
+import path from "path";
+import { CONFIG } from "./config";
 
+async function main() {
+  const trainData = await fs.readFile(path.join(...CONFIG.JSON_DATA_PATH, "singapore_train_station.json"), { encoding: "utf-8" });
+  const data = JSON.parse(trainData);
+  const trainLine = data.map((train: any) => train.mrt_line_english);
+  await fs.writeFile(path.join(...CONFIG.JSON_DATA_PATH, "singapore_train_line.json"), JSON.stringify([...new Set(trainLine)], null, 2));
 };
+
 
 main();  
